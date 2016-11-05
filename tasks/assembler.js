dd04c0d0ea07acc2729738055c63c0af82d478cf
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2016-11-05 16:31:03 +0100

    Flatten out functions inside assembler task

--- tasks/assembler.js
+++ tasks/assembler.js
@@ -46,98 +46,90 @@ var getFromUrl = function(location) {
     });
 };
 
-module.exports = function(grunt) {
-    var cache = {};
+function contentHash(data) {
+    var sha384 = crypto.createHash('sha384');
+    sha384.update(data);
+    return "sha384-" + sha384.digest('base64');
+}
 
-    function getIntegrationHash(href, data) {
-        var promise;
-        var result;
-        var hash = function(data) {
-            var sha384 = crypto.createHash('sha384');
-            sha384.update(data);
-            return "sha384-" + sha384.digest('base64');
-        };
-        var cacheHash = function(data) {
-            var sha = crypto.createHash('sha');
-            sha.update(data);
-            return sha.digest('hex');
-        };
-        var fileCache = __dirname + "/../build/cache/" + cacheHash(href).substr(0, 16);
-        data = data || {};
-        if (data && data.etag) {
-            promise = getFromUrl({location: href, etag: data.etag, fileCache: fileCache})
-                .then(function(res) {
-                    data.integrity = hash(res.data);
-                    return new Promise(function(done) {
-                        done(data);
-                    });
-                });
-        } else {
-            promise = getFromUrl({location: href, fileCache: fileCache})
-                .then(function(res) {
-                    if (typeof res.headers.etag === "string") {
-                        data.etag = res.headers.etag;
-                    }
-                    data.integrity = hash(res.data);
-                    return new Promise(function(done) {
-                        done(data);
-                    });
-                });
+function cacheHash(fileName) {
+    var sha = crypto.createHash('sha');
+    sha.update(fileName);
+    return sha.digest('hex');
+}
+
+function getIntegrationHash(href, data) {
+    data = data || {};
+
+    return getFromUrl({
+        location: href,
+        etag: data.etag,
+        fileCache: __dirname + "/../build/cache/" + cacheHash(href).substr(0, 16)
+    }).then(function(res) {
+        if (typeof res.headers.etag === "string") {
+            data.etag = res.headers.etag;
         }
-        return promise;
+        data.integrity = contentHash(res.data);
+        return new Promise(function(done) {
+            done(data);
+        });
+    });
+}
+
+
+var cache = {};
+function processDependencies(settings) {
+    var dependenciesFile = "build/data/dependencies.json";
+    var promises = [];
+
+    if (cache.integrity === undefined) {
+        if (fs.existsSync(dependenciesFile)) {
+            cache.integrity = JSON.parse(fs.readFileSync(dependenciesFile, {encoding: 'utf-8'}));
+        } else {
+            cache.integrity = {};
+        }
     }
 
-    function processDependencies(settings) {
-        var dependenciesFile = "build/data/dependencies.json";
-        var promises = [];
-
-        if (cache.integrity === undefined) {
-            if (grunt.file.isFile(dependenciesFile)) {
-                cache.integrity = JSON.parse(grunt.file.read(dependenciesFile));
-            } else {
-                cache.integrity = {};
-            }
-        }
-
-        var updateData = function(url) {
-            return function(data) {
-                cache.integrity[url] = data;
-            };
+    var updateData = function(url) {
+        return function(data) {
+            cache.integrity[url] = data;
         };
+    };
 
-        var stylesheets = settings.stylesheets;
-        var scripts = settings.scripts;
+    var stylesheets = settings.stylesheets;
+    var scripts = settings.scripts;
 
+    for (var i = 0; i < stylesheets.length; i++) {
+        promises.push(getIntegrationHash(
+            stylesheets[i],
+            cache.integrity[stylesheets[i]]
+        ).then(updateData(stylesheets[i])));
+    }
+    for (var j = 0; j < scripts.length; j++) {
+        promises.push(getIntegrationHash(
+            scripts[j],
+            cache.integrity[scripts[j]]
+        ).then(updateData(scripts[j])));
+    }
+    return Promise.all(promises).then(function() {
+        fs.writeFileSync(dependenciesFile, JSON.stringify(cache.integrity));
+        settings.stylesheets = [];
+        settings.scripts = [];
         for (var i = 0; i < stylesheets.length; i++) {
-            promises.push(getIntegrationHash(
-                stylesheets[i],
-                cache.integrity[stylesheets[i]]
-            ).then(updateData(stylesheets[i])));
+            cache.integrity[stylesheets[i]].href = stylesheets[i];
+            settings.stylesheets.push(cache.integrity[stylesheets[i]]);
         }
         for (var j = 0; j < scripts.length; j++) {
-            promises.push(getIntegrationHash(
-                scripts[j],
-                cache.integrity[scripts[j]]
-            ).then(updateData(scripts[j])));
+            cache.integrity[scripts[j]].href = scripts[j];
+            settings.scripts.push(cache.integrity[scripts[j]]);
         }
-        return Promise.all(promises).then(function() {
-            grunt.file.write(dependenciesFile, JSON.stringify(cache.integrity));
-            settings.stylesheets = [];
-            settings.scripts = [];
-            for (var i = 0; i < stylesheets.length; i++) {
-                cache.integrity[stylesheets[i]].href = stylesheets[i];
-                settings.stylesheets.push(cache.integrity[stylesheets[i]]);
-            }
-            for (var j = 0; j < scripts.length; j++) {
-                cache.integrity[scripts[j]].href = scripts[j];
-                settings.scripts.push(cache.integrity[scripts[j]]);
-            }
-            return new Promise(function(done) {
-                done(settings);
-            });
+        return new Promise(function(done) {
+            done(settings);
         });
-    }
+    });
+}
 
+module.exports = function(grunt) {
     grunt.registerMultiTask('assembler', function(){
         var done = this.async();
         var options = this.options({
