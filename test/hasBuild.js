f7efea2fb874acb223ac48a6ab3dd0c918d4d3f9
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2016-11-08 14:58:26 +0100

    Fix broken build

--- test/hasBuild.js
+++ test/hasBuild.js
@@ -2,6 +2,6 @@ var fs = require("fs");
 
 describe("hasBuild", function() {
     it("Should have index.html in the docs folder", function() {
-        fs.accessSync(__dirname + "/../docs/index.html");
+        fs.existsSync(__dirname + "/../docs/index.html");
     });
 });
